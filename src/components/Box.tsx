56806dd8d281737389220ea5815e6348ebd4dd36
Dmitrii Kirshanov <ext.dkirshanov@tinkoff.ru>
2022-01-08 16:58:15 +0200

    init

---
+++ src/components/Box.tsx
@@ -0,0 +1,25 @@
+import * as THREE from "three";
+import React, { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const Box: React.FC<JSX.IntrinsicElements["mesh"]> = (props) => {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame(() => (ref.current.rotation.x += 0.01));
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+export default Box;
