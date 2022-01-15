458f9246e0693bfd6cc25d56f842b786fbdf0938
Dmitrii Kirshanov <ext.dkirshanov@tinkoff.ru>
2022-01-15 16:04:29 +0200

    add css modules ts support

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       components: path.resolve(__dirname, "src/components"),
     },
   },
+  css: {
+    modules: { localsConvention: "camelCase" },
+  },
+  plugins: [react()],
 });
