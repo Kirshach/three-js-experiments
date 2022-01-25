a4a67901b0b2176aa4018919ef73b0afbf8510b3
Dmitrii Kirshanov <ext.dkirshanov@tinkoff.ru>
2022-01-25 22:35:31 +0200

    delete mode store

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { subscribeToAppMode } from "src/stores/app/subscriptions";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-subscribeToAppMode();
